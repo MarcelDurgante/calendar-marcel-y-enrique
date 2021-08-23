e3f13dced28b0e175890fa3ed4f18886a92fb9a7
MarcelDurgante <marceldur@gmail.com>
2021-08-23 12:05:13 +0200

    created months arrays, added calendar days and controllers pre and pos date

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ let currentYear = today.getFullYear();
 let currentMonth = today.getMonth();
 let currentDate = today.getDay();
 let currentDay = today.getDate();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let controllers = document.getElementById("controllers");
+
+controllers.textContent = currentDay + " " + months[currentMonth] + " " + currentYear
+
