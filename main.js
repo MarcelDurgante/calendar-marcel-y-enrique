4d47979ae3bfc3aa807d8bbe2568faa8763c7c00
Enrique Hidalgo <koko.freelance@gmail.com>
2021-08-23 10:39:22 +0100

    Started Calendar

---
+++ main.js
@@ -0,0 +1,7 @@
+// ------ VARIABLES -----
+
+let today = new Date();
+let currentYear = today.getFullYear();
+let currentMonth = today.getMonth();
+let currentDate = today.getDay();
+let currentDay = today.getDate();
