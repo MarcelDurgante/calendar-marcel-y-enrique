14aa27e67743ff3372645c6345d232fce8dc1281
Enrique Hidalgo <koko.freelance@gmail.com>
2021-08-26 17:16:17 +0100

    Added Event Modal,Months and Years change

--- main.js
+++ main.js
@@ -21,15 +21,32 @@ const months = [
   "December",
 ];
 
+// ------ VARIABLES FOR ARROWS -----
+
 let controllers = document.getElementById("controllers");
 let nextArrow = document.getElementById("posDate");
 let prevArrow = document.getElementById("preDate");
 
+// ------ VARIABLES FOR MODAL -----
+
+let newEvent = document.getElementById("newEvent");
+let modal = document.getElementById("modal");
+let closeBtn = document.getElementById("closeBtn");
+let cancel = document.getElementById("cancel");
+let endDate = document.getElementById("endDate");
+let chooseEndDate = document.getElementById("chooseEndDate");
+let reminder = document.getElementById("reminder");
+let chooseRemind = document.getElementById("chooseRemind");
+
 // ------ TO SHOW CURRENT MONTH AND YEAR -----
 
-controllers.textContent = months[currentMonth] + " " + currentYear;
+function showCurrentDate() {
+  controllers.textContent = months[currentMonth] + " " + currentYear;
+}
+showCurrentDate();
 
 // ------ TO CHANGE MONTH MONTH AND YEAR -----
+
 nextArrow.addEventListener("click", () => {
   if (currentMonth === 11) {
     currentMonth = 0;
@@ -37,9 +54,9 @@ nextArrow.addEventListener("click", () => {
   } else {
     currentMonth++;
   }
-  // ------ TO SHOW CURRENT MONTH AND YEAR -----
 
-  controllers.textContent = months[currentMonth] + " " + currentYear;
+  // ------ TO SHOW CURRENT MONTH AND YEAR -----
+  showCurrentDate();
 });
 
 prevArrow.addEventListener("click", () => {
@@ -51,7 +68,40 @@ prevArrow.addEventListener("click", () => {
   }
   // ------ TO SHOW CURRENT MONTH AND YEAR -----
 
-  controllers.textContent = months[currentMonth] + " " + currentYear;
+  showCurrentDate();
+});
+
+// ----- OPEN & CLOSE EVENTS MODAL ----
+
+newEvent.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+cancel.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "none";
+});
+
+// ----- CHOOSE ENDATE AND REMINDER ----
+
+endDate.addEventListener("change", () => {
+  if (endDate.checked) {
+    chooseEndDate.style.display = "block";
+  } else {
+    chooseEndDate.style.display = "none";
+  }
+});
+
+reminder.addEventListener("change", () => {
+  if (reminder.checked) {
+    chooseRemind.style.display = "block";
+  } else {
+    chooseRemind.style.display = "none";
+  }
 });
 
 // ------- BRUCE IS PRINTING DAYS -----
@@ -59,14 +109,3 @@ prevArrow.addEventListener("click", () => {
 let calendarDays = document.getElementById("calendarDays");
 let startDate = new Date(currentYear, currentMonth, 1);
 let daysQuantity = new Date(currentYear, currentMonth + 1, 0).getDate();
-
-function renderCalendar() {
-  for (let index = 1; index < daysQuantity.length; index++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("day");
-    newDiv.innerHTML = "Hello";
-    calendarDays.appendChild(newDiv);
-  }
-}
-
-renderCalendar();
